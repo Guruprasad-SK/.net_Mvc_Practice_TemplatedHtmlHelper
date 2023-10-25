d0d5c310ad224b0cfe083bee78ed66f64a53c879
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2023-10-25 18:42:38 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TemplatedHTMLHelper.Models;
+
+namespace TemplatedHTMLHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Employee employee = new Employee()
+            {
+                Id = 1,
+                Name="Vinay",
+                Email="vinay@gmail.com",
+                isOnline=true,
+                DateOfBirth=DateTime.Now
+
+            };
+            return View(employee);
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            return View(employee);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TemplatedHTMLHelper.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Display(Name ="please enter your name")]
+        public string Name { get; set; }
+        public string   Email { get; set; }
+        public bool isOnline { get; set; }
+        [Display(Name="Date of birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime BirthTime { get; set; }
+    }
+}
